1ce4611be65258afbee5afb21fe2feb4dbb5c63c
nenaddjordjevic <nenad.djordjevic@basiq.io>
2025-03-04 16:07:55 +0100

    fix: add print statement to display file contents in main function

--- app/main.py
+++ app/main.py
@@ -19,6 +19,8 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
+    print(file_contents)
+
     error = False
     i = 0
     line = 1
