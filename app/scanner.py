f01fba16b7bae01747fe0289100e33015cf47796
nenaddjordjevic <nenad.djordjevic@basiq.io>
2025-03-27 14:13:57 +0100

    fix string

--- app/scanner.py
+++ app/scanner.py
@@ -130,19 +130,15 @@ class Scanner:
             if self.peek() == "\n":
                 self.line += 1
             self.advance()
-
+    
         # If we reach the end without finding a closing quote
         if self.is_at_end():
-            value = self.source[
-                self.start + 1 : self.current
-            ]  # Extract the valid portion
-            self.add_token(TokenType.STRING, value)  # Add the valid portion as a token
             error(self.line, "Unterminated string.")  # Report the error
             return  # Stop further tokenization
-
+    
         # The closing quote
         self.advance()
-
+    
         # Trim the surrounding quotes
         value = self.source[self.start + 1 : self.current - 1]
         self.add_token(TokenType.STRING, value)
