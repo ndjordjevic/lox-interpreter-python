eff1141a06a06c6fb6b42cb3f63e4603861de6e6
nenaddjordjevic <nenad.djordjevic@basiq.io>
2025-04-10 12:19:52 +0200

    remove evaluator

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from .scanner import Scanner
 from .parser import Parser
 from .ast_printer import AstPrinter
 from .utils import error_state
-from .expr import Binary, Grouping, Literal, Unary, Evaluator
+from .expr import Binary, Grouping, Literal, Unary
 from .token import Token
 from .token_type import TokenType
